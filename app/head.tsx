99ba87508545413c177ef4b515a629f66f739743
ELATTAR-Ayoub <elattarayoub000@gmail.com>
2023-02-19 17:52:34 +0100

    fix UI 0.2

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,8 @@ export default function Head() {
     <>
       <title>GPT-Travel - Plan your next journey with chat-GPT</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="GPT-Travel - Plan your next journey with chat-GPT" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="GPT-Travel - Plan your trip to your favorite city!" />
+      <link rel="icon" href="/GPT-Travel_logo.ico" />
     </>
   )
 }
