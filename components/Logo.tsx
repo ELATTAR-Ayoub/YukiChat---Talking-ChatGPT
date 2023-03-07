95eaf6f70f6e9a5d4d1cc51630b22e8f6aabd1fa
ELATTAR-Ayoub <elattarayoub000@gmail.com>
2023-03-07 11:58:23 +0100

    delete useles kits

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -10,7 +10,7 @@ const Loader = () => {
     <div className={`flex justify-end items-end gap-2 cursor-pointer`}>
         {/* <Image src={'./logo.svg'} alt={'logo'} width={26} height={26} /> */}
 
-        <div className='relative font-bold text-sm text-accent-color-71 top-1'>
+        <div className='relative font-bold text-sm md:text-base text-accent-color-71 top-1'>
           YukiChat - Talking GPT
         </div>
     </div>
